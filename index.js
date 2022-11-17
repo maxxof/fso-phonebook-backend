30c9905d97effd423f0b4fa335331c16ead5abda
maxxof <maxim.egorov.soccer@gmail.com>
2022-11-17 16:46:36 +0200

    Ex 3.5 and 3.6 done

--- index.js
+++ index.js
@@ -60,6 +60,39 @@ app.delete("/api/persons/:id", (req, res) => {
     res.status(204).end()
 })
 
+const generateId = () => {
+    return Math.random() * (9999999999 - 1) + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    const isDuplicate = persons.some(person => person.name === body.name)
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: "name is missing"
+        })
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: "number is missing"
+        })
+    } else if (isDuplicate) {
+        return res.status(400).json({
+            error: "name must be unique"
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
