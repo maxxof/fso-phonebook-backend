a131f9e295e52a4e28b4243038096c5059a12b95
maxxof <maxim.egorov.soccer@gmail.com>
2022-11-19 13:27:20 +0200

    uibuild

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 morgan.token('body', (req, res) => {
     if (JSON.stringify(req.body) === "{}") {
         return ' '
