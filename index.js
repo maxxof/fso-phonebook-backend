5e2a3c32e7326625592114e5a62ffe53ae702f62
maxxof <maxim.egorov.soccer@gmail.com>
2022-11-20 15:39:18 +0200

    Ex 3.14 done

--- index.js
+++ index.js
@@ -20,28 +20,7 @@ morgan.token('body', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      id: 1,
-      name: "Arto Hellas", 
-      number: "040-123456"
-    },
-    { 
-      id: 2,
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
-    },
-    { 
-      id: 3,
-      name: "Dan Abramov", 
-      number: "12-43-234345"
-    },
-    { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
-    }
-]
+let persons = []
 
 const Info = () => {
     const time = new Date()
@@ -79,14 +58,8 @@ app.delete("/api/persons/:id", (req, res) => {
     res.status(204).end()
 })
 
-const generateId = () => {
-    return Math.floor(Math.random() * 999999999999999999)
-}
-
 app.post('/api/persons', (req, res) => {
     const body = req.body
-
-    const isDuplicate = persons.some(person => person.name === body.name)
 
     if (!body.name) {
         return res.status(400).json({
@@ -96,20 +69,16 @@ app.post('/api/persons', (req, res) => {
         return res.status(400).json({
             error: "number is missing"
         })
-    } else if (isDuplicate) {
-        return res.status(400).json({
-            error: "name must be unique"
-        })
     }
 
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 const PORT = process.env.PORT
